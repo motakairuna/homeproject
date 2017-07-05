c87b9f00f374f1794e75b7c84dfdc11dff89e1a9
Iryna <motakairuna@gmail.com>
2017-07-05 10:46:48 +0000

    Initial

---
+++ js/form.js
@@ -0,0 +1,25 @@
+// JavaScript File
+$(function(){
+    $('#btn-send').click(sendForm);
+});
+
+
+function sendForm(e){
+  e.preventDefault();
+  $.ajax({
+      url: "https://formspree.io/motakairuna@gmail.com", 
+      method: "POST",
+      data: {
+      name:$('#client-name').val(),
+      email:$('#client-email').val(),
+      tel:$('#client-tel').val(),
+      comment:$('#comment').val()
+  },
+      dataType: "json"
+  })
+   .done(function(){
+    $('form').html('<h1>Дякую</h1>') ;  
+  });
+}
+
+
